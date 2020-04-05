88a4dbe0debfe95283a717f442a90ebcb1a70b4a
wagnerDestro <wagnerDestro@users.noreply.github.com>
2020-04-05 10:16:19 -0300

    Perguntas dinâmicas
    Comecei o processo de puxar as perguntas de um txt na cena Perguntas

---
+++ Assets/perguntaDinamica.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class perguntaDinamica : MonoBehaviour
+{
+
+    public string nomeArquivo = "teste.txt";
+    public Text pergunta;
+
+    void Start()
+    {
+       pergunta = GameObject.Find("Pergunta").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var sr = new StreamReader(Application.dataPath + "/" + nomeArquivo);
+        var ConteudoArquivo = sr.ReadToEnd();
+        sr.Close();
+        pergunta.text = "ConteudoArquivo";
+    }
+}
