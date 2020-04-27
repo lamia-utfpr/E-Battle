27d2599186a7ce1cd988425be4fa04f47ffc5bcf
Heitor Vilas Boas <heitor.cerezine@gmail.com>
2020-04-27 13:06:25 -0400

    Organização do código
    O código foi organizado em blocos e comentado para melhor entendimento

--- Assets/OnClick.cs
+++ Assets/OnClick.cs
@@ -36,9 +36,88 @@ public class OnClick : MonoBehaviour
         move3 = GameObject.FindGameObjectWithTag("Player3").GetComponent<MvP3>();
         move4 = GameObject.FindGameObjectWithTag("Player4").GetComponent<MvP4>();*/
     }
-    public void telaInicial()
+
+    //Este bloco de códigos contém todos os códigos responsáveis por mudança de cenas.
+
+    
+    public void telaInicial()   // transição entre a tela inicial e a tela do tabuleiro do jogo
     {
         SceneManager.LoadScene("Tabuleiro", LoadSceneMode.Single); //Ao clicar no botão, ele sai da tela inicial(tela 0) e vai para tela do jogo(tela 1)
+    }
+
+
+    public void TelaDePerguntas()  // transição entre a tela do tabuleiro e a tela de apresentação de perguntas
+    {
+        SceneManager.LoadSceneAsync("Perguntas", LoadSceneMode.Single);
+    }
+
+
+    public void CriarPerguntas()  //  transição entre a tela inicial e a tela onde será criado o tema do txt.
+    {
+        SceneManager.LoadScene("Criação de perguntas", LoadSceneMode.Single);
+    }
+
+
+    public void AcertoDePergunta()  //  transição após o jogador acertar a pergunta, voltando para o tabuleiro
+    {
+        SceneManager.LoadScene("Tabuleiro", LoadSceneMode.Single);
+    }
+
+
+    public void ErroDePergunta()  //  transição após o jogador errar a pergunta, voltando para o tabuleiro
+    {
+        SceneManager.LoadScene("Tabuleiro", LoadSceneMode.Single);
+    }
+    // Fim do bloco de transição de perguntas
+
+
+
+    // Bloco de código que lidam com a customização dos txt
+
+
+    public void CriarTema()  // Este bloco é responsavel pela criação do txt com o tema criado pelo professor
+    {
+
+        temas = tema.text;
+        //determina o local de criação das perguntas, registrando o txt com o nome do tema
+        path = Application.dataPath + "/" + temas + ".txt";
+        PlayerPrefs.SetString("path", path);
+
+        //Se o arquivo tema não existir, vai criar um arquivo com o tema
+        if (!File.Exists(path))
+        {
+            File.WriteAllText(path, "Perguntas com o tema " + temas + "\n");
+        }
+        SceneManager.LoadScene("Criação da Pergunta e Resposta", LoadSceneMode.Single);
+    }
+
+
+    public void adicionarPergunta()  //  Este bloco eziste na tela "Criação da pergunta e Resposta" e ele adiciona a pergunta e a resposta correta ao txt
+    {
+        path = PlayerPrefs.GetString("path");
+        File.AppendAllText(path, pergunta.text + "; " + respostaCerta.text + ";\n");
+        SceneManager.LoadScene("Criação da Pergunta e Resposta", LoadSceneMode.Single);
+    }
+
+    public void adicionarAlternativa()  //  Este bloco adiciona a pergunta e resposta correta ao txt e após isso vai para a tela de adicionar uma alternativa extra
+    {
+        path = PlayerPrefs.GetString("path");
+        File.AppendAllText(path, pergunta.text + "; " + respostaCerta.text + "; ");
+        SceneManager.LoadSceneAsync("Criação de Alternativa", LoadSceneMode.Single);
+    }
+
+    public void adicionarAlternativaErrada()  // Este bloco existe na tela Criação de alternativa e adiciona uma alternativa incorreta ao txt
+    {
+        path = PlayerPrefs.GetString("path");
+        File.AppendAllText(path, alternativaErrada.text + "; \n");
+        SceneManager.LoadScene("Criação da Pergunta e Resposta", LoadSceneMode.Single);
+    }
+
+    public void adicionarAlternativaExtra()  // Este bloco existe na tela Criação de alternativa e adiciona uma alternativa incorreta ao txt e volta para a mesma tela
+    {
+        path = PlayerPrefs.GetString("path");
+        File.AppendAllText(path, alternativaErrada.text + "; ");
+        SceneManager.LoadScene("Criação de Alternativa", LoadSceneMode.Single);
     }
 
 
@@ -53,73 +132,11 @@ public class OnClick : MonoBehaviour
             Debug.Log("B");
     }*/
 
-    public void TelaDePerguntas() {
-        SceneManager.LoadSceneAsync("Perguntas", LoadSceneMode.Single);
-    }
-
-
-    public void CriarPerguntas()
-    {
-        SceneManager.LoadScene("Criação de perguntas", LoadSceneMode.Single);
-    }
-
-    public void CriarTema()
-    {
-        
-        temas = tema.text;
-        //determina o local de criação das perguntas, registrando o txt com o nome do tema
-        path = Application.dataPath +"/"+ temas + ".txt";
-        PlayerPrefs.SetString("path",path);
-
-        //Se o arquivo tema não existir, vai criar um arquivo com o tema
-        if (!File.Exists(path))
-        {
-            File.WriteAllText(path,"Perguntas com o tema "+temas + "\n");
-        }
-        SceneManager.LoadScene("Criação da Pergunta e Resposta", LoadSceneMode.Single);
-    }
 
     public void Sair(){
         Application.Quit();
     }
 
-    public void adicionarPergunta()
-    {
-        path = PlayerPrefs.GetString("path");
-        File.AppendAllText(path, pergunta.text +"; "+ respostaCerta.text+";\n");
-        SceneManager.LoadScene("Criação da Pergunta e Resposta", LoadSceneMode.Single);
-    }
-
-    public void adicionarAlternativa()
-    {
-        path = PlayerPrefs.GetString("path");
-        File.AppendAllText(path, pergunta.text + "; " + respostaCerta.text + "; ");
-        SceneManager.LoadSceneAsync("Criação de Alternativa",LoadSceneMode.Single);
-    }
-
-    public void adicionarAlternativaErrada()
-    {
-        path = PlayerPrefs.GetString("path");
-        File.AppendAllText(path, alternativaErrada.text + "; \n");
-        SceneManager.LoadScene("Criação da Pergunta e Resposta", LoadSceneMode.Single);
-    }
-
-    public void adicionarAlternativaExtra()
-    {
-        path = PlayerPrefs.GetString("path");
-        File.AppendAllText(path, alternativaErrada.text + "; ");
-        SceneManager.LoadScene("Criação de Alternativa", LoadSceneMode.Single);
-    }
-
-    public void AcertoDePergunta()
-    {
-        SceneManager.LoadScene("Tabuleiro", LoadSceneMode.Single);
-    }
-
-    public void ErroDePergunta()
-    {
-        SceneManager.LoadScene("Tabuleiro", LoadSceneMode.Single);
-    }
     
     public void TaskOnClick() {
         //SceneManager.LoadScene(3, LoadSceneMode.Additive);      //O indice 2 é sobre a cena que faz as perguntas.
