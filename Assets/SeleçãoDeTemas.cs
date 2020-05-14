55eab6e6b75a1f405675099f4af56f261150523c
Heitor Vilas Boas <heitor.cerezine@gmail.com>
2020-05-14 14:35:10 -0400

    Seleção de temas
    Foi feito a tela para seleção dos temas, porém sem a adição de botões

---
+++ Assets/SeleçãoDeTemas.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class SeleçãoDeTemas : MonoBehaviour
+{
+
+    FileInfo[] temas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DirectoryInfo dir = new DirectoryInfo(Application.dataPath + "/temas/");
+        temas = dir.GetFiles("*.txt");
+        //var fileInfo = Directory.getFiles(Application.dataPath + "/temas/","*.txt");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //DirectoryInfo dir = new DirectoryInfo(Application.dataPath + "/temas/");
+        //FileInfo[] temas = dir.GetFiles("*.txt");
+        foreach (FileInfo f in temas)
+        {
+            Debug.Log(f);
+        }
+    }
+}
