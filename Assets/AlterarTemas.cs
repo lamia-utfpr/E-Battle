197edeba28563696d49cb70bf850d938e795c7e2
Heitor Vilas Boas <heitor.cerezine@gmail.com>
2020-05-18 13:51:30 -0400

    Selecionar Temas
    Foi criada a tela de selecão de temas, para alterar os temas ja criados e para selecionar o tema que sera utilizado na partida

---
+++ Assets/AlterarTemas.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+public class AlterarTemas : MonoBehaviour
+{
+
+    string diretorio;
+    public RawImage arquivo;
+
+    public void abrirExplorer()
+    {
+        diretorio = EditorUtility.OpenFilePanel("Overwrite with png", Application.dataPath + "/temas/", "txt");
+        abrirArquivo();
+    }
+
+    void abrirArquivo()
+    {
+        if(diretorio != null)
+        {
+            mostrarArquivo();
+        }
+    }
+
+    void mostrarArquivo()
+    {
+        WWW www = new WWW("file:///" + diretorio);
+        arquivo.texture = www.texture;
+        Debug.Log(PlayerPrefs.GetString("path"));
+        PlayerPrefs.SetString("path",diretorio);
+        Debug.Log(PlayerPrefs.GetString("path"));
+    }
+
+}
