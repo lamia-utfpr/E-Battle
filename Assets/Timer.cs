93fc6a68c08dd8036485a96f3ae71df198f89f06
Heitor Vilas Boas <heitor.cerezine@gmail.com>
2020-05-20 13:49:29 -0400

    Timer
    Foi adicionado um timer na tela de perguntas, e um powerup para aumento do tempo de resposta

---
+++ Assets/Timer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+
+    public int contador;
+    public Text texto;
+
+    void Start()
+    {
+        StartCoroutine(Temporizador());
+    }
+
+    IEnumerator Temporizador()
+    {
+        while (contador > 0)
+        {
+            texto.text = contador.ToString();
+
+            yield return new WaitForSeconds(1f);
+
+            contador--;
+        }
+    }
+
+    public void adicionarTempo()
+    {
+        texto.GetComponent<Timer>().contador += 10;
+    }
+   
+}
