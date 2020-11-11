3574b72ed53b59d5d93834a2d76f069158cb5d64
wagnerDestro <wagner.leandro28@outlook.com>
2020-11-11 15:54:45 -0300

    Alteração na tela de inserir perguntas
    Alterações foram feitas na tela de inserir perguntas.

--- E-Battle/Assets/Scripts/slider_value.cs
+++ E-Battle/Assets/Scripts/slider_value.cs
@@ -7,44 +7,135 @@ public class slider_value : MonoBehaviour {
 
   public Slider sliderUI;
   private Text Valor_slider;
+  
   public InputField input1;
+  public Text placeholder1;
+  public Text texto_usuario1;
+
   public InputField input2;
+  public Text placeholder2;
+  public Text texto_usuario2;
+
   public InputField input3;
+  public Text placeholder3;
+  public Text texto_usuario3;
+
   public InputField input4;
+  public Text placeholder4;
+  public Text texto_usuario4;
+
 
   void Start (){
-    Valor_slider = GetComponent<Text>();
-    ShowSliderValue();
+     Valor_slider = GetComponent<Text>();
+
+    placeholder1.text = "";
+    placeholder2.text = "";
+    placeholder3.text = "";
+    placeholder4.text = "";
+
+    input1.interactable = false;
+    input2.interactable = false;
+    input3.interactable = false;
+    input4.interactable = false;
   }
 
   public void ShowSliderValue () {
-
     string sliderMessage = "Quantidade de alternativas: "+ sliderUI.value;
     Valor_slider.text = sliderMessage;
+    setActive(sliderUI.value);
   }
 
 
 
   void update(){
-      setActive();
+    ShowSliderValue();
   }
 
 
-  void setActive(){
-      if (Valor_slider.text == "0"){
-
-      }
-      else{
-           
-        int i = 0;
-
-        for (i = 1; i < 5; i++){
-            if (i <= Convert.ToInt32(Valor_slider.text)){
-                input1.ActivateInputField();
-            }
-
-        }
-      }
+  void setActive(float valor_slider){
      
+      if ((int)valor_slider == 0){
+        placeholder1.text = "";
+        placeholder2.text = "";
+        placeholder3.text = "";
+        placeholder4.text = "";
+
+        texto_usuario1.text = "";
+        texto_usuario2.text = "";
+        texto_usuario3.text = "";
+        texto_usuario4.text = "";
+
+        input1.interactable = false;
+        input2.interactable = false;
+        input3.interactable = false;
+        input4.interactable = false;
+      }
+      else if ((int) valor_slider == 1){
+          placeholder1.text = "Alternativa 1";
+          input1.interactable = true;
+
+          input2.interactable = false;
+          input3.interactable = false;
+          input4.interactable = false;
+
+
+          texto_usuario2.text = "";
+          texto_usuario3.text = "";
+          texto_usuario4.text = "";
+
+
+          placeholder2.text = "";
+          placeholder3.text = "";
+          placeholder4.text = "";
+      }
+      else if ((int) valor_slider == 2){
+          placeholder1.text = "Alternativa 1";
+          input1.interactable = true;
+
+          placeholder2.text = "Alternativa 2";
+          input2.interactable = true;
+
+          input3.interactable = false;
+          input4.interactable = false;
+
+          texto_usuario3.text = "";
+          texto_usuario4.text = "";
+
+          placeholder3.text = "";
+          placeholder4.text = "";
+      }
+      else if ((int) valor_slider == 3){
+          placeholder1.text = "Alternativa 1";
+          input1.interactable = true;
+
+          placeholder2.text = "Alternativa 2";
+          input2.interactable = true;
+
+          input3.interactable = true;
+          placeholder3.text = "Alternativa 3";
+
+
+          input4.interactable = false;
+
+          texto_usuario4.text = "";
+          
+          placeholder4.text = "";
+      }
+      else if ((int) valor_slider == 4){
+          placeholder1.text = "Alternativa 1";
+          input1.interactable = true;
+
+          placeholder2.text = "Alternativa 2";
+          input2.interactable = true;
+
+          input3.interactable = true;
+          placeholder3.text = "Alternativa 3";
+
+          input4.interactable = true;
+          placeholder4.text = "Alternativa 4";
+          
+      }
   }
+
+
 }
