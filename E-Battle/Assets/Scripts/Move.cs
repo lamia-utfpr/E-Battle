e53f997e4967a0f30505d0195025e29a3bf7ff90
RafaAngelin <rafael938543@gmail.com>
2021-09-14 23:33:05 -0300

    Animações na tela inicial

---
+++ E-Battle/Assets/Scripts/Move.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    [SerializeField] Transform[] Positions;
+    [SerializeField] float ObjectSpeed;
+    int NextPosIndex;
+    Transform NextPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        NextPos = Positions[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Mover();
+    }
+
+    void Mover()
+    {
+
+        if (transform.position == NextPos.position)
+        {
+            NextPosIndex++;
+            if (NextPosIndex >= Positions.Length)
+            {
+                NextPosIndex = 0;
+            }
+            NextPos = Positions[NextPosIndex];
+
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, NextPos.position, ObjectSpeed * Time.deltaTime);
+        }
+
+    }
+
+}
